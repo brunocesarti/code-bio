95224679376a207384b7e3cc22afa7cb32383119
Bruno César <brunocesarmg@gmail.com>
2021-09-15 12:37:37 -0300

    Adicionando campos permitidos em model Saldo #6

--- app/Models/SaldoModel.php
+++ app/Models/SaldoModel.php
@@ -14,7 +14,7 @@ class SaldoModel extends Model
     protected $returnType           = 'array';
     protected $useSoftDeletes       = false;
     protected $protectFields        = true;
-    protected $allowedFields        = [];
+    protected $allowedFields        = ['conta', 'valor', 'moeda','operacao'];
 
     // Dates
     protected $useTimestamps        = false;
