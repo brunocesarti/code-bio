f60f6371b2797ec467e7208c21f1e9be2328c657
Bruno César <brunocesarmg@gmail.com>
2021-09-15 11:58:22 -0300

    Atualizando controller saque com operacoes #4

--- app/Controllers/Saque.php
+++ app/Controllers/Saque.php
@@ -3,6 +3,9 @@
 namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+
+use App\Models\SaqueModel;
 
 class Saque extends ResourceController
 {
@@ -13,7 +16,17 @@ class Saque extends ResourceController
      */
     public function index()
     {
-        //
+        $model = new SaqueModel();
+      
+        $data = $model->findAll();
+      
+        $response = [
+            'status' => 200,
+            'error' => null,
+            'messages' => "Saques Encontrados",
+            "data" => $data,
+        ];
+        return $this->respond($response);
     }
 
     /**
@@ -23,7 +36,21 @@ class Saque extends ResourceController
      */
     public function show($id = null)
     {
-        //
+        $model = new SaqueModel();
+      
+        $data = $model->where(['id' => $id])->first();
+      
+        if ($data) {
+            $response = [
+                'status' => 200,
+                'error' => null,
+                'messages' => "Saques Encontrados",
+                "data" => $data,
+            ];
+            return $this->respond($response);
+        } else {
+            return $this->failNotFound('Nenhum saque encontrado com o id: ' . $id);
+        }
     }
 
     /**
@@ -43,7 +70,23 @@ class Saque extends ResourceController
      */
     public function create()
     {
-        //
+        $model = new SaqueModel();
+
+        $data = [
+            'conta' => $this->request->getVar('conta'),
+            'valor' => $this->request->getVar('valor'),
+            'moeda' => $this->request->getVar('moeda'),
+        ];
+
+        $model->insert($data);
+
+        $response = [
+            'status' => 200,
+            'error' => null,
+            'messages' => "Saque realizado com sucesso!",
+        ];
+      
+        return $this->respondCreated($response);
     }
 
     /**
@@ -53,7 +96,7 @@ class Saque extends ResourceController
      */
     public function edit($id = null)
     {
-        //
+        return $this->failNotFound('Não é possivel editar um saque.');
     }
 
     /**
@@ -63,7 +106,7 @@ class Saque extends ResourceController
      */
     public function update($id = null)
     {
-        //
+        return $this->failNotFound('Não é possivel atualizar um saque.');
     }
 
     /**
@@ -73,6 +116,7 @@ class Saque extends ResourceController
      */
     public function delete($id = null)
     {
-        //
+        return $this->failNotFound('Não é possivel apagar um saque.');
+
     }
 }
